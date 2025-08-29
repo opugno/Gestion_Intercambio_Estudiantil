39fb5e94ea14eeaaba4f5b8b469a35cffe9a558d
Opugno <maura.gonzalez.s@mail.pucv.cl>
2025-08-28 20:42:36 -0400

    Commit 1: Subida inicial del proyecto desde NetBeans

---
+++ src/main/java/com/mycompany/intercambio_estudiantil/Intercambio_estudiantil.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author desiredheaven
+ */
+public class Intercambio_estudiantil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
