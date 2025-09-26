a514152a59a0024ae41408d122a368b8dd7f4c08
opugno <maura.gonzalez.s@mail.pucv.cl>
2025-09-26 10:26:09 -0300

    Clase Tramite
    agrege la clase que no salia y corregi el LocalDte en Documento subido

--- src/main/java/com/mycompany/intercambioEstudiantil/DocumentoSubido.java
+++ src/main/java/com/mycompany/intercambioEstudiantil/DocumentoSubido.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.intercambioEstudiantil;
+import java.time.LocalDate;
 
 public class DocumentoSubido 
 {
--- src/main/java/com/mycompany/intercambioEstudiantil/Tramite.java
+++ src/main/java/com/mycompany/intercambioEstudiantil/Tramite.java
@@ -1,6 +1,75 @@
 package com.mycompany.intercambioEstudiantil;
 
+import java.util.*;
+
+//cada tramite tiene los documentos subidos por el estudiante para postular a un convenio
 public class Tramite 
 {
+    public enum Estado {EN_PROCESO, COMPLETO}
     
+    //ATRIBUTOS
+    private String idTramite;
+    private Estudiante estudiante;
+    private Map<TipoDocumento, DocumentoSubido> documento = new HashMap<>();
+    private Estado estado =  Estado.EN_PROCESO;
+    
+    //CONSTRUCTOR
+    public Tramite(String idTramite, Estudiante estudiante)
+    {
+        this.idTramite = idTramite;
+        this.estudiante = estudiante;
+    }
+    
+    //SETTERS Y GETTERS
+    public String getIdTramite(){
+        return idTramite;
+    }
+    public void setIdTramite(String idTramite){
+        this.idTramite = idTramite;
+    }
+    
+    public Estudiante getEstudiante(){
+        return estudiante;
+    }
+    public void setEstudiante(Estudiante estudiante)
+    {
+        this.estudiante = estudiante;
+    }
+    
+    public Map<TipoDocumento, DocumentoSubido> getDocumentos(){
+        return documento;
+    }
+    public void setDocumento(Map<TipoDocumento, DocumentoSubido> documento){
+        this.documento = documento;
+    }
+    
+    public Estado getEstado(){
+        return estado;
+    }
+    public void setEstado(Estado estado){
+        this.estado = estado;
+    }
+    
+    
+    //METODOS y sobrecarga de metodos
+    public void subirDocumento(TipoDocumento tipo, String nombreArchivo) 
+    {
+        documento.put(tipo, new DocumentoSubido(tipo, nombreArchivo, java.time.LocalDate.now()));
+    }
+    public void subirDocumento(String tipo, String nombreArchivo) 
+    {
+        subirDocumento(TipoDocumento.valueOf(tipo), nombreArchivo);
+    }
+
+    // Si se cumplen todos los requisitos del convenio
+    public boolean estaCompleto(Set<TipoDocumento> requisitos) 
+    {
+        for (TipoDocumento req : requisitos) 
+        {
+            if (!documento.containsKey(req)) 
+                return false;
+        }
+        return true;
+    }
 }
+
